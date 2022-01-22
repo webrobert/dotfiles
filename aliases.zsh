c1b247cd304f862a9c2db6b60541767339fecd47
webrobert <webrobert@gmail.com>
2022-01-22 09:43:40 -0800

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,8 @@
 # Shortcuts
-# alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
-# alias reloadshell="source $HOME/.zshrc"
-# alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-# alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
