bf598598e2b1a993cb8cf5a92908c581cf77a4d9
webrobert <webrobert@gmail.com>
2022-01-22 10:41:41 -0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,5 +5,5 @@ echo "Cloning repositories..."
 SITES=$HOME/Development/Websites
 
 # Personal
-git clone git@github.com:driesvints/driesvints.com.git $SITES/driesvints.com
-git clone git@github.com:driesvints/learn-laravel-cashier.com.git $SITES/learn-laravel-cashier.com
+git clone git@github.com:webrobert/graham.git $SITES/graham
+# git clone git@github.com:driesvints/learn-laravel-cashier.com.git $SITES/learn-laravel-cashier.com
