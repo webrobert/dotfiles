8a1918b6065e702c08a9d36664e506f9248ecf2e
webrobert <webrobert@gmail.com>
2022-02-21 10:32:59 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -19,6 +19,10 @@ fi
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
+# I think 
+# zsh needs to be restarted here 
+# to pickup the newly saved $varabiles
+
 # Update Homebrew recipes
 brew update
 
@@ -26,32 +30,11 @@ brew update
 brew tap homebrew/bundle
 brew bundle --file $DOTFILES/Brewfile
 
-# Set default MySQL root password and auth type
-# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
-# Install PHP extensions with PECL
-# pecl install imagick redis swoole
-
-# Install global Composer packages
-# /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose
-
-# Install Laravel Valet
-# $HOME/.composer/vendor/bin/valet install
-
 # Create a Sites directory
 mkdir $HOME/Sites
-# mkdir $HOME/Development/Websites
-
-# Create sites subdirectories
-# mkdir $HOME/Sites/blade-ui-kit
-# mkdir $HOME/Sites/eventsauce
-# mkdir $HOME/Sites/laravel
 
 # Clone Github repositories
 $DOTFILES/clone.sh
-
-# Symlink the Mackup config file to the home directory
-# ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source $DOTFILES/.macos
