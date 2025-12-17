4f5a03dd70b768e1281adc00d212d083c92d2378
webrobert <webrobert@gmail.com>
2025-12-17 06:30:00 -0800

    Modify zsh aliases for better error handling & lando
    Updated aliases for improved functionality and error handling.

--- aliases.zsh
+++ aliases.zsh
@@ -1,38 +1,29 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias copyssh="pbcopy < ${HOME}/.ssh/id_ed25519.pub 2>/dev/null || echo 'No ~/.ssh/id_ed25519.pub found'"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias ll='command -v gls >/dev/null && gls -AhlFo --color --group-directories-first || ls -AhlFo'
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
-# alias compile="commit 'compile'"
-# alias version="commit 'version'"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
 alias sites="cd $HOME/Sites"
 
-# Laravel
-alias a="php artisan"
-alias fresh="php artisan migrate:fresh --seed"
-alias seed="php artisan db:seed"
-
-# PHP
-# alias cfresh="rm -rf vendor/ composer.lock && composer i"
-# alias composer="php -d memory_limit=-1 /opt/homebrew/bin/composer"
+# Laravel (Lando-first)
+alias a="lando artisan"
+alias fresh="lando artisan migrate:fresh --seed"
+alias seed="lando artisan db:seed"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
 
-# Docker
-# alias docker-composer="docker-compose"
-alias spin="~/.dotfiles/spin-laravel.sh" # with a lando file
-
-# SQL Server
-# alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
+# Project helpers
+alias spin="~/.dotfiles/spin-new-laravel.sh"
+alias spinclone="~/.dotfiles/spin-clone-laravel.sh"
 
 # Git
 alias gst="git status"
@@ -42,7 +33,7 @@ alias gl="git log --oneline --decorate --color"
 alias amend="git add . && git commit --amend --no-edit"
 alias commit="git add . && git commit -m"
 alias diff="git diff"
-alias force="git push --force"
+alias force="git push --force-with-lease"
 alias nuke="git clean -df && git reset --hard"
 alias pop="git stash pop"
 alias pull="git pull"
